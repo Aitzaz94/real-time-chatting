649ba65c443e342aa040235cb6c043dbeb20152d
Aitzaz94 <32670716+Aitzaz94@users.noreply.github.com>
2023-08-07 00:24:53 +0500

    chatapp with web socket implementation

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates_uniqueness_of :username
+
+    scope :all_except, -> (user) { where.not(:id => user)}
+
+    after_create_commit { broadcast_append_to "users" }
+
+    has_many :messages
+end
